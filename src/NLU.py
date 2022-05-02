4cafd26b4b8f796c31bd0b85fa75fd76af813c75
SunMin0 <ksm5742@gmail.com>
2022-05-02 17:49:41 +0900

    초기설정 누락된 부분
    2022 05 02 PM 5:49

---
+++ src/NLU.py
@@ -0,0 +1,85 @@
+import torch
+from src.dataset import MakeDataset
+from src.model import MakeEmbed, textCNN, DAN, BiLSTM_CRF
+
+class NaturalLanguageUnderstanding:
+    def __init__(self):
+        self.dataset = MakeDataset()
+        self.embed = MakeEmbed()
+        self.embed.load_word2vec()
+
+        self.weights = self.embed.word2vec.wv.vectors
+        self.weights = torch.FloatTensor(self.weights)
+
+        self.intent_clsf = textCNN(self.weights, 256, [3,4,5], 0.5, len(self.dataset.intent_label))
+        self.ood_clsf = DAN(self.weights, 256, 0.5, 2)
+        self.entity_recog = BiLSTM_CRF(self.weights, self.dataset.entity_label, 256, 128)
+
+    def model_load(self):
+        intent_pretrain_path = "./nlp/pretrained/intent_clsf_99.819_steps_4.pt"
+        entity_pretrain_path = "./nlp/pretrained/entity_recog_97.192_steps_3.pt"
+        ood_pretrain_path = "./nlp/pretrained/ood_clsf_99.801_steps_8.pt"
+
+        self.intent_clsf.load_state_dict(torch.load(intent_pretrain_path))
+        self.entity_recog.load_state_dict(torch.load(entity_pretrain_path))
+        self.ood_clsf.load_state_dict(torch.load(ood_pretrain_path))
+
+        self.intent_clsf.eval()
+        self.entity_recog.eval()
+        self.ood_clsf.eval()
+
+    def predict(self, query):
+        tokens = self.dataset.tokenize(query)
+        q2idx = self.embed.query2idx(tokens)
+        x = self.dataset.prep.pad_idx_sequencing(q2idx)
+        x = torch.tensor(x)
+        f = self.ood_clsf(x.unsqueeze(0))
+        ood = torch.argmax(f).tolist()
+
+        if ood:
+            f = self.intent_clsf(x.unsqueeze(0))
+            intent = self.dataset.intents[torch.argmax(f).tolist()]
+        else:
+            intent = 'ood'
+
+        f = self.entity_recog(x.unsqueeze(0))
+
+        mask = torch.where(x>0, torch.tensor([1.]), torch.tensor([0.])).type(torch.uint8)
+
+        predict = self.entity_recog.decode(f, mask.view(1, -1))
+
+        return intent, predict
+
+    def convert_nlu_result(self, query, intent, predict):
+        NLU_result = {
+            "INTENT" : "",
+            "SLOT" : []
+        }
+        x_token = query.split()
+        slots = []
+        BIE = []
+        prev = "";
+        for i, slot in enumerate([self.dataset.entitys[p] for p in predict[0]]):
+            name = slot[2:]
+            if "S-" in slot:
+                if BIE != []:
+                    slots.append(prev[2:]+"^"+" ".join(BIE))
+                    BIE = []
+                slots.append(name+"^"+x_token[i])
+            elif "B-" in slot:
+                BIE.append(x_token[i])
+                prev = slot
+            elif "I-" in slot and ("B" in prev or "I" in prev) :
+                BIE.append(x_token[i])
+                prev = slot
+            elif "E-" in slot and ("I" in prev or "B" in prev):
+                BIE.append(x_token[i])
+                slots.append(name+"^"+" ".join(BIE))
+            else:
+                if BIE != []:
+                    slots.append(prev[2:]+"^"+" ".join(BIE))
+                    BIE = []
+
+        NLU_result["INTENT"] = intent
+        NLU_result["SLOT"] = slots
+        return NLU_result
---
+++ src/dataset.py
@@ -0,0 +1,165 @@
+import json
+import random
+import pandas as pd
+import torch
+from torch.utils.data import TensorDataset
+import torch.utils.data as data
+
+class Preprocessing:
+    def __init__(self, max_len=20):
+        self.max_len = max_len
+        self.PAD = 0
+
+    def pad_idx_sequencing(self, q_vec):
+        q_len = len(q_vec)
+        diff_len = q_len - self.max_len
+        if diff_len > 0:
+            q_vec = q_vec[:self.max_len]
+            q_len = self.max_len
+        else:
+            pad_vac = [0] * abs(diff_len)
+            q_vec += pad_vac
+
+        return q_vec
+
+
+class EntityDataset(data.Dataset):
+    def __init__(self, x_tensor, y_tensor, lengths):
+        super(EntityDataset, self).__init__()
+        self.x = x_tensor
+        self.y = y_tensor
+        self.lengths = lengths
+
+    def __getitem__(self, index):
+        return self.x[index], self.y[index], self.lengths[index]
+
+    def __len__(self):
+        return len(self.x)
+
+
+class MakeDataset:
+    def __init__(self):
+        self.intent_data_dir = "./intent_data.csv"
+        self.intent_label_dir = "./intent_label.json"
+        self.entity_label_dir = "./entity_label.json"
+        self.entity_data_dir = "./entity_data.csv"
+        self.ood_data_dir = "./ood_data.csv"
+        self.intent_label = self.load_intent_label()
+        self.entity_label = self.load_entity_label()
+        self.prep = Preprocessing()
+
+
+    # 의도 레이블 데이터 불러오기
+    def load_intent_label(self):
+        f = open(self.intent_label_dir, encoding='UTF-8')
+        intent_label = json.loads(f.read())
+        self.intents = list(intent_label.keys())
+        return intent_label
+
+    def load_entity_label(self):
+        f = open(self.entity_label_dir, encoding='UTF-8')
+        entity_label = json.loads(f.read())
+        self.entitys = list(entity_label.keys())
+        return entity_label
+
+    # 띄어쓰기를 기준으로 문장 자르기
+    def tokenize(self, sentence):
+        return sentence.split()
+
+    # 전달 받은 데이터셋을 띄어쓰기 기준으로 자른 문장의 리스트로 만들기
+    def tokenize_dataset(self, dataset):
+        token_dataset = []
+        for data in dataset:
+            token_dataset.append(self.tokenize(data))
+        return token_dataset
+
+    # 문장을 idx로 변환하고 레이블도 숫자로 변환
+    def make_intent_dataset(self, embed):
+        intent_dataset = pd.read_csv(self.intent_data_dir)
+        labels = [self.intent_label[label] for label in intent_dataset["label"].to_list()]
+        intent_querys = self.tokenize_dataset(intent_dataset["question"].tolist())
+        dataset = list(zip(intent_querys, labels))
+        intent_train_dataset, intent_test_dataset = self.word2idx_dataset(dataset, embed)
+        return intent_train_dataset, intent_test_dataset
+
+    def make_entity_dataset(self, embed):
+        entity_dataset = pd.read_csv(self.entity_data_dir)
+        entity_querys = self.tokenize_dataset(entity_dataset["question"].tolist())
+        labels = []
+        for label in entity_dataset["label"].to_list():
+            temp = []
+            for entity in label.split():
+                temp.append(self.entity_label[entity])
+            labels.append(temp)
+        dataset = list(zip(entity_querys, labels))
+        entity_train_dataset, entity_test_dataset = self.word2idx_dataset(dataset, embed)
+        return entity_train_dataset, entity_test_dataset
+
+    def make_ood_dataset(self, embed):
+        intent_dataset = pd.read_csv(self.intent_data_dir)
+        ood_dataset = pd.read_csv(self.ood_data_dir)
+        intent_dataset = pd.concat([intent_dataset, ood_dataset])
+        labels = []
+        for label in intent_dataset["label"].to_list():
+            if label == "OOD":
+                labels.append(0)
+            else:
+                labels.append(1)
+
+        intent_querys = self.tokenize_dataset(intent_dataset["question"].tolist())
+        dataset = list(zip(intent_querys, labels))
+        intent_train_dataset, intent_test_dataset = self.word2idx_dataset(dataset, embed)
+        return intent_train_dataset, intent_test_dataset
+
+    def word2idx_dataset(self, dataset, embed, train_ratio = 0.8):
+        question_list, label_list, lengths = [], [], []
+        flag = True
+        random.shuffle(dataset)
+        for query, label in dataset:
+            q_vec = embed.query2idx(query)
+            lengths.append(len(q_vec))
+            q_vec = self.prep.pad_idx_sequencing(q_vec)
+
+            question_list.append(torch.tensor([q_vec]))
+
+            if isinstance(label, list):
+                label = self.prep.pad_idx_sequencing(label)
+                label_list.append(label)
+                flag=False
+            else:
+                label_list.append(torch.tensor([label]))
+
+        x = torch.cat(question_list)
+        if flag :                       # Intent Dataset
+            y = torch.cat(label_list)
+        else:                           # Entity Dataset
+            y = torch.tensor(label_list)
+
+        x_len = x.size()[0]
+        y_len = y.size()[0]
+        if (x_len == y_len) :
+            train_size = int(x_len * train_ratio)
+            train_x = x[:train_size]
+            train_y = y[:train_size]
+
+            test_x = x[train_size+1:]
+            test_y = y[train_size+1:]
+
+        if flag:
+            train_dataset = TensorDataset(train_x, train_y)
+            test_dataset = TensorDataset(test_x, test_y)
+        else :
+            train_length = lengths[:train_size]
+            test_length = lengths[train_size+1:]
+
+            train_dataset = EntityDataset(train_x, train_y, train_length)
+            test_dataset = EntityDataset(test_x, test_y, test_length)
+
+        return train_dataset, test_dataset
+
+    def make_embed_dataset(self, ood=False):
+        embed_dataset = pd.read_csv(self.intent_data_dir)
+        embed_dataset = embed_dataset["question"].to_list()
+        embed_dataset = self.tokenize_dataset(embed_dataset)
+
+        return embed_dataset
